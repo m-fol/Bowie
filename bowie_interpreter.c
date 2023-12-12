5e0d7982ed795e5a3236701362dd7714760474e8
Mary Fol <139060140+m-fol@users.noreply.github.com>
2023-12-13 00:22:25 +0200

    Create bowie_interpreter.c

---
+++ bowie_interpreter.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+
+#define MEMORY_SIZE 30000
+
+int main() {
+    char tape[MEMORY_SIZE] = {0};
+    int ptr = 0;
+
+    char instruction;
+    while (1) {
+        instruction = getchar();
+
+        if (instruction == EOF || instruction == '\n') {
+            break; // End of program
+        }
+
+        switch (instruction) {
+            case 'Z':
+                tape[ptr] = 0; // Ziggy Stardust
+                break;
+            case 'M':
+                ++tape[ptr]; // Major Tom
+                break;
+            case 'S':
+                --tape[ptr]; // Space Oddity
+                break;
+            case 'H':
+                putchar(tape[ptr]); // Starman
+                break;
+            case 'L':
+                tape[ptr] = getchar(); // Life on Mars?
+                break;
+            case 'C':
+                if (tape[ptr] == 0) {
+                    int nesting = 1;
+                    while (nesting > 0) {
+                        instruction = getchar();
+                        if (instruction == 'C') {
+                            nesting++;
+                        } else if (instruction == 'H') {
+                            nesting--;
+                        }
+                    }
+                }
+                break;
+            case 'R':
+                if (tape[ptr] != 0) {
+                    int nesting = 1;
+                    while (nesting > 0) {
+                        instruction = getchar();
+                        if (instruction == 'R') {
+                            nesting++;
+                        } else if (instruction == 'E') {
+                            nesting--;
+                        }
+                    }
+                }
+                break;
+        }
+    }
+
+    return 0;
+}
