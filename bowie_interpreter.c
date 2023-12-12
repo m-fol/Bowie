c5653099103b90aa23fc972078dfe2233c175a47
Mary Fol <139060140+m-fol@users.noreply.github.com>
2023-12-12 23:05:03 +0000

    a

--- bowie_interpreter.c
+++ bowie_interpreter.c
@@ -1,63 +1,21 @@
 #include <stdio.h>
 
+#include <stdio.h>
+
 #define MEMORY_SIZE 30000
 
 int main() {
     char tape[MEMORY_SIZE] = {0};
     int ptr = 0;
-
     char instruction;
 
     while ((instruction = getchar()) != EOF) {
-        //printf("Executing instruction: %c\n", instruction);
-
         switch (instruction) {
-            case 'Z':
-                tape[ptr] = 0; // Ziggy Stardust
-                break;
-            case 'M':
-                ++tape[ptr]; // Major Tom
-                break;
-            case 'S':
-                --tape[ptr]; // Space Oddity
-                break;
             case 'H':
-                putchar(tape[ptr]); // Starman
+                putchar(tape[ptr]);
                 break;
             case 'L':
-                tape[ptr] = getchar(); // Life on Mars?
-                break;
-            case 'C':
-                if (tape[ptr] == 0) {
-                    int nesting = 1;
-                    while (nesting > 0) {
-                        instruction = getchar();
-                        //printf("Inside loop C: %c\n", instruction);
-                        if (instruction == 'C') {
-                            nesting++;
-                        } else if (instruction == 'H') {
-                            nesting--;
-                        } else if (instruction == EOF) {
-                            break; // Break out of the loop if EOF is encountered
-                        }
-                    }
-                }
-                break;
-            case 'R':
-                if (tape[ptr] != 0) {
-                    int nesting = 1;
-                    while (nesting > 0) {
-                        instruction = getchar();
-                        //printf("Inside loop R: %c\n", instruction);
-                        if (instruction == 'R') {
-                            nesting++;
-                        } else if (instruction == 'E') {
-                            nesting--;
-                        } else if (instruction == EOF) {
-                            break; // Break out of the loop if EOF is encountered
-                        }
-                    }
-                }
+                tape[ptr] = getchar();
                 break;
         }
     }
