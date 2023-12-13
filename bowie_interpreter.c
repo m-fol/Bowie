ea7e35013b050899ee5f721cda188f49fa34fcb1
Mary Fol <139060140+m-fol@users.noreply.github.com>
2023-12-13 02:07:07 +0200

    Update bowie_interpreter.c

--- bowie_interpreter.c
+++ bowie_interpreter.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <ctype.h>
 
 #define MEMORY_SIZE 30000
 
@@ -6,13 +7,14 @@ int main() {
     char tape[MEMORY_SIZE] = {0};
     int ptr = 0;
     int instruction;
+    char nextChar;
 
     while ((instruction = getchar()) != EOF) {
         if (isspace(instruction)) {
             continue;
-        char nextChar; // Variable to store the next character
+        }
 
-        printf("Processing: %c\n", instruction);
+        //printf("Processing: %c\n", instruction);
 
         switch (instruction) {
             case 'Z':
@@ -37,13 +39,14 @@ int main() {
                     int nesting = 1;
                     while (nesting > 0) {
                         nextChar = getchar();
-                        printf("Inside loop C: %c\n", nextChar);
+                        // printf("Inside loop C: %c\n", nextChar);
                         if (nextChar == 'C' || nextChar == 'c') {
                             nesting++;
                         } else if (nextChar == 'h' || nextChar == 'H') {
                             nesting--;
                         } else if (nextChar == EOF) {
-                            break; // Break out of the loop if EOF is encountered
+                            fprintf(stderr, "Error: Unexpected EOF\n");
+                            return 1;  // Exit with an error code
                         }
                     }
                 }
@@ -53,13 +56,16 @@ int main() {
                     int nesting = 1;
                     while (nesting > 0) {
                         nextChar = getchar();
-                        printf("Inside loop R: %c\n", nextChar);
+                        // printf("Inside loop R: %c\n", nextChar);
                         if (nextChar == 'R' || nextChar == 'r') {
                             nesting++;
                         } else if (nextChar == 'e' || nextChar == 'E') {
                             nesting--;
+                        } else if (nextChar == 'N' || nextChar == 'n') {
+                            // Ignore 'N' and 'n' to prevent ENQ interpretation
                         } else if (nextChar == EOF) {
-                            break; // Break out of the loop if EOF is encountered
+                            fprintf(stderr, "Error: Unexpected EOF\n");
+                            return 1;  // Exit with an error code
                         }
                     }
                 }
